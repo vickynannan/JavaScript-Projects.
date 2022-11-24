fa905de5b7d96875442baeb4744e19f78009aaa0
vickynannan <107215611+vickynannan@users.noreply.github.com>
2022-11-24 13:26:41 +0800

    Add files via upload

---
+++ Vicky's Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function My_First_Function() {
+    var str="This is a button";
+    document.getElementById("Button").innerHTML = str;//call for string
+}
+function myFunction() {
+    var sentence = " I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
