1fd49bd312f0a0b5791b92732dc13f2c67b0fc49
vickynannan <107215611+vickynannan@users.noreply.github.com>
2022-11-24 12:12:05 +0800

    Add files via upload

---
+++ Vicky's Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+window.alert("Hello,How are you?");//this is a window alert
+document.write("How are you today?!");//this is document write method
+
+document.write("\"This is the beginning of the string,"+"and this is the end of the string.\"");//it's a concatenate string
+document.write(23+35);//it's expression
