a6202932d72ff4e0199029f6659306dda092c3cd
vickynannan <107215611+vickynannan@users.noreply.github.com>
2022-12-06 02:28:58 +0800

    Add files via upload

--- Vicky's Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Vicky's Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,10 @@
 window.alert("Hello,How are you?");//this is a window alert
-document.write("How are you today?!");//this is document write method
+document.write("\"Where are you living?"+"This is a nice place.If you do\'t mind.\"" + "David asked.");
 
+document.write("How are you today?!");//this is document write method
+var A = "This is the beginning of the string";
+document.write(A);
+var B = "and this is the end of the string."
+document.write(B);
 document.write("\"This is the beginning of the string,"+"and this is the end of the string.\"");//it's a concatenate string
 document.write(23+35);//it's expression
